e6496e21a434e551018112abcd9b189430a50e5a
Alex Macleod <alex@macleod.io>
2015-11-10 00:58:33 +0000

    Hook config groundwork, issues fully working

--- hooks.rb
+++ hooks.rb
@@ -18,6 +18,7 @@ $bot = Cinch::Bot.new do
     c.user = $config['irc']['user']
     c.password = $config['irc']['password']
     c.channels = $config['irc']['channels']
+    c.modes = $config['irc']['modes']
   end
 
   on :message, "ping" do |m|
@@ -45,13 +46,13 @@ post '/' do
   begin
     send "receive_#{event}", data
   rescue NameError
-    return halt 501, "Not Implemented"
+    return halt 501, "No method: #{event}"
   end
-  return halt 200, "Success"
+  return halt 200
 end
 
 def verify_signature(payload_body)
-  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), $config['hooks']['secret'], payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), $config['secret'], payload_body)
   return halt 500, "Signature mismatch" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
 
@@ -63,9 +64,7 @@ def receive_push(d)
   branch = d['ref'].split('/').last
   repo = d['repository']['name']
 
-  puts shorten(d['compare'])
-
-  say "[#{fmt_repo repo}] #{fmt_name d['pusher']['name']} pushed #{fmt_num d['commits'].count} new commit#{d['commits'].count == 1 ? '' : 's'} to #{fmt_branch branch}"
+  say "[#{fmt_repo repo}] #{fmt_name d['pusher']['name']} pushed #{fmt_num d['commits'].count} new commit#{d['commits'].count == 1 ? '' : 's'} to #{fmt_branch branch}:#{fmt_url shorten d['compare']}"
   d['commits'][0..2].each do |commit|
     if commit['message'].include? "\n"
       message = commit['message'].split("\n").first + "..."
@@ -76,37 +75,49 @@ def receive_push(d)
   end
 end
 
+def receive_issues(d)
+  return halt 202, "#{d['action']} not enabled" unless $config['hooks']['pull_request']['actions'].include? d['action']
+  say "[#{fmt_repo d['repository']['name']}] #{fmt_name d['issue']['user']['login']} #{d['action']} issue ##{d['issue']['number']}: #{d['issue']['title']}#{fmt_url shorten d['issue']['html_url']}"
+end
+
+def receive_issue_comment(d)
+  say "[#{fmt_repo d['repository']['name']}] #{fmt_name d['issue']['user']['login']} commented on issue ##{d['issue']['number']}: #{d['issue']['title']}#{fmt_url shorten d['comment']['html_url']}"
+end
+
+def receive_pull_request(d)
+
+end
+
 def shorten(url)
   uri = URI.parse(url)
   response = Net::HTTP.post_form(URI.parse("http://git.io"), {"url" => uri})
-
-  puts "\n\nCode: #{response.code}\n\nBody: #{response.body}\n\n"
+  return response['location']
 end
 
 def fmt_url(s)
-  "\00302\037#{s}\017"
+  "\00302 #{s}"
 end
 
 def fmt_repo(s)
-  "\00313#{s}\017"
+  "\00313#{s}\003"
 end
 
 def fmt_name(s)
-  "\00315#{s}\017"
+  "\00315#{s}\003"
 end
 
 def fmt_branch(s)
-  "\00306#{s}\017"
+  "\00306#{s}\003"
 end
 
 def fmt_tag(s)
-  "\00306#{s}\017"
+  "\00306#{s}\003"
 end
 
 def fmt_hash(s)
-  "\00314#{s}\017"
+  "\00314#{s}\003"
 end
 
 def fmt_num(s)
-  "\002#{s}\017"
+  "\002#{s}\003"
 end
