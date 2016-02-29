06c067d58f65263bc0930b09079ecf92f089e8f8
Alex Macleod <alex@macleod.io>
2016-02-29 00:23:07 +0000

    AKK mitigation method

--- hookbot.rb
+++ hookbot.rb
@@ -20,7 +20,11 @@ $bot = Cinch::Bot.new do
       data = JSON.parse(RestClient.get("https://api.github.com/repos/#{CONFIG[:github][:repo]}/issues/#{issue}",
                                        params: { access_token: CONFIG[:github][:token] }),
                         symbolize_names: true)
-      m.reply "[#{data[:state] == 'open' ? fmt_good('open') : fmt_bad('closed')}] #{data.key?(:pull_request) ? 'Pull request' : 'Issue'} ##{data[:number]}: #{data[:title]}#{fmt_url shorten data[:html_url]}"
+      if issue == '1010'
+        m.reply "[#{fmt_bad('annoying')}] Issue #1010: Nobody cares"
+      else
+        m.reply "[#{data[:state] == 'open' ? fmt_good('open') : fmt_bad('closed')}] #{data.key?(:pull_request) ? 'Pull request' : 'Issue'} ##{data[:number]}: #{data[:title]}#{fmt_url shorten data[:html_url]}"
+      end
     end
   end
 end
