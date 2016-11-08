b8a53c60b521e465c43329d7c22ac0ca196ad41f
Alex Macleod <alex@macleod.io>
2016-11-08 14:58:26 +0000

    Replace return with next in proc

--- hookbot.rb
+++ hookbot.rb
@@ -15,7 +15,7 @@ $bot = Cinch::Bot.new do
   end
 
   on :message, /\B#\d{2,}\b/ do |m|
-    return if m.user.host.include? '/bot/'
+    next if m.user.host.include? '/bot/'
     m.message.scan(/\B#\K\d{2,}\b/).first(3).each do |issue|
       data = JSON.parse(RestClient.get("https://api.github.com/repos/#{CONFIG[:github][:repo]}/issues/#{issue}",
                                        params: { access_token: CONFIG[:github][:token] }),
